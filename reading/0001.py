48e0c3ca7c761a950ee4908c9618e6ca9b78189d
serm-tape <serm.thm@gmail.com>
2021-01-24 18:40:10 +0700

    Add code reading challenge

---
+++ reading/0001.py
@@ -0,0 +1,14 @@
+# What this function do?
+
+def doSomething(numbers):
+    if len(numbers) != 2:
+        return None
+    else
+        a = numbers[0]
+        numbers[0] = numbers[1]
+        numbers[1] = a
+
+    return numbers
+
+# doSomething([1, 2])
+    
